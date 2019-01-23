2380bc0a24e9e17a413b515067715cabbcec805e
turneps403 <turneps403@mail.ru>
2019-01-23 16:45:34 +0300

    - fix case

--- content.js
+++ content.js
@@ -21,7 +21,7 @@ document.addEventListener ("keydown", function (zEvent) {
         var text = document.getSelection().toString();
         if (text !== '') {
             // console.log(text);
-            text = text.match(/\w+/)[0];
+            text = text.match(/\w+/)[0].toLowerCase();
             if (text !== '') {
                 let voice_url = 'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=' + encodeURI(text) +  '&tl=en&total=1&idx=0&textlen=' + text.length;
                 new Audio(voice_url).play();
