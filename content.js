c1581bfa2c42ce788ba248fe0fc577fb906ecf4c
turneps403 <turneps403@mail.ru>
2019-01-24 12:33:08 +0300

    - fallback added

--- content.js
+++ content.js
@@ -20,11 +20,20 @@ document.addEventListener ("keydown", function (zEvent) {
     if (zEvent.code == "Backquote" && zEvent.shiftKey) {
         var text = document.getSelection().toString();
         if (text !== '') {
-            // console.log(text);
             text = text.match(/\w+/)[0].toLowerCase();
             if (text !== '') {
                 let voice_url = 'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=' + encodeURI(text) +  '&tl=en&total=1&idx=0&textlen=' + text.length;
-                new Audio(voice_url).play();
+                let au = new Audio();
+                au.onerror = () => {
+                    console.log('error');
+                    let one_char = text.substring(0, 1);
+                    let three_chars = text.substring(0, 3).padEnd(3, '_');
+                    let five_chars = text.substring(0, 5).padEnd(5, '_');
+                    voice_url = 'https://dictionary.cambridge.org/media/english/us_pron/'+ encodeURIComponent(one_char) +'/'+ encodeURIComponent(three_chars) +'/'+ encodeURIComponent(five_chars) +'/'+ encodeURIComponent(text) +'.mp3';
+                    new Audio(voice_url).play();
+                };
+                au.src = voice_url;
+                au.play();
             }
         }
     } 
